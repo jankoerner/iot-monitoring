f044cf1308ac591e2f11c70807799cf0dcb2dacb
Jan Körner <jankoerner.1998@gmail.com>
2023-09-18 22:42:25 +0200

    added deployment of rpi-monitoring

--- host/deploy.py
+++ host/deploy.py
@@ -1,33 +1,14 @@
 import argparse
-import csv
-import os
 import paramiko
 from scp import SCPClient
 
-iot_folder = '~/iot-monitoring/'
-            
-def create_ssh(ip, user, pw):
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(ip, username=user, password=pw) 
-    
-    return ssh_client
+from utils import *
 
-def startup(connections):
-    for connection in connections.items():
-        ip = connection[0]
-        user = connection[1][0]
-        pw = connection[1][1]
-        
-        client = create_ssh(ip,user,pw)      
-        client.exec_command(f"cd {iot_folder}/; (nohup ./startup.sh &); cd ~")
-        client.close()
+iot_folder = '~/iot-monitoring/'
 
 def copy_folder(src, dest, connections):
     for connection in connections.items():
-        ip = connection[0]
-        user = connection[1][0]
-        pw = connection[1][1]
+        ip,user,pw = extract_conn_infos(connection)
         
         client = create_ssh(ip,user,pw)
         client.exec_command('mkdir -p ' + dest)
@@ -37,24 +18,35 @@ def copy_folder(src, dest, connections):
         
         scp.close()
         client.close()
+        
+def install_monitoring(src, connections):
+    copy_folder(src, iot_folder , connections)
+    
+    for connection in connections.items():
+        ip,user,pw = extract_conn_infos(connection)
+        client = create_ssh(ip,user,pw)
+        
+        stdin, stdout, stderr = client.exec_command(f"cd {iot_folder}/monitoring; (sudo -S apt-get -y install ./*)")
+        stdin.write("solo + \n")
+        stdin.flush()
+        
+        stdin, stdout, stderr = client.exec_command("sudo /usr/share/rpimonitor/scripts/updatePackagesStatus.pl")
+        stdin.write("solo + \n")
+        stdin.flush()        
+        
+        client.close()
 
+            
 def main():
     parser = argparse.ArgumentParser(description="Script to deploy data and binaries to the pi's")
     parser.add_argument('-c', '--connections', help='Path to the file with connection information')
-    parser.add_argument('-d', '--data', help='Path to folder with the data-sets')
     parser.add_argument('-b', '--binaries', help='Path to the binaries of the algorithms')
-    parser.add_argument('-s', '--startup', help='If the startup script should run', action='store_true')
+    parser.add_argument('-d', '--data', help='Path to the folder with the data-sets')
+    parser.add_argument('-m', '--monitoring', help='Path to the folder of the monitoring binaries')
     
     args = parser.parse_args()
     
-    connections = {}
-    with open(args.connections, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            ip = row['ip']
-            user = row['user']
-            pw = row['password']            
-            connections[ip] = (user, pw)
+    connections = read_connection_file(args.connections)
     
     if args.data:
         copy_folder(args.data, iot_folder + "data", connections)
@@ -62,9 +54,8 @@ def main():
     if args.binaries:
         copy_folder(args.binaries, iot_folder + "binaries", connections)
     
-    if args.startup:
-        startup(connections)
-        
-
+    if args.monitoring:
+        install_monitoring(args.monitoring, connections)
+    
 if __name__ == "__main__":
     main()
---
+++ host/utils.py
@@ -0,0 +1,30 @@
+import csv
+import paramiko
+
+iot_folder = '~/iot-monitoring/'
+
+def create_ssh(ip, user, pw):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(ip, username=user, password=pw) 
+    
+    return ssh_client
+
+def extract_conn_infos(connection):
+    ip = connection[0]
+    user = connection[1][0]
+    pw = connection[1][1]
+    
+    return ip,user,pw
+
+def read_connection_file(src):
+    connections = {}
+    with open(src, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            ip = row['ip']
+            user = row['user']
+            pw = row['password']            
+            connections[ip] = (user, pw)
+    
+    return connections
